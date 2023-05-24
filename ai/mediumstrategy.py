901c9134191a299a48624e29b6eaf618fbee8b75
Nikola Jolovic <nikolajolovic02@gmail.com>
2023-05-24 11:13:43 +0200

    Add medium strategy

--- ai/mediumstrategy.py
+++ ai/mediumstrategy.py
@@ -1,3 +1,14 @@
-def MediumStrategy(object):
+from game.util import get_possible_moves, calculate_board_position
+from ai.board_evaluation import evaluate_board
+
+class MediumStrategy(object):
     def get_move(self, board: list[list[int]], turn: int) -> tuple[int,int]:
-        return (0, 0)
+        moves = get_possible_moves(board, turn)
+        best_move = moves[0]
+        best_evaluation = turn * evaluate_board(calculate_board_position(board, turn, best_move))
+        for move in moves[1:]:
+            current_evaluation = turn * evaluate_board(calculate_board_position(board, turn, move))
+            if current_evaluation > best_evaluation:
+                best_move = move
+                best_evaluation = current_evaluation
+        return best_move
